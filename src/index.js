96918781914a5fdd086acaa1046df53434297cd3
Rafael Audy <rafael.audy@gmail.com>
2019-02-26 18:42:42 +0530

    Adds mutation

--- src/index.js
+++ src/index.js
@@ -1,20 +1,48 @@
-const { GraphQLServer } = require('graphql-yoga');
+const { GraphQLServer } = require("graphql-yoga");
 
-const typeDefs = `
-    type Query {
-        info: String!
-    }
-`;
+const feed = [
+  { id: 1, description: "desc", url: "url" },
+  { id: 2, description: "desc2", url: "url2" }
+];
+let idCount = feed.length;
 
 const resolvers = {
-    Query: {
-        info: () => `This is the API of a Hackernews Clone`
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => feed
+  },
+  Mutation: {
+    post: (parent, { url, description }) => {
+      const link = {
+        id: idCount++,
+        url,
+        description
+      };
+
+      feed.push(link);
+      return link;
     }
+  }
 };
 
 const server = new GraphQLServer({
-    typeDefs,
-    resolvers
+  typeDefs: `
+    type Query {
+      info: String!
+      feed: [Link!]!
+    }
+    
+    type Mutation {
+      post(url: String!, description: String!): Link!
+    }
+    
+    type Link {
+      id: ID!
+      description: String!
+      url: String!
+    }
+  `,
+  resolvers
 });
 
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+server.start(() => console.log(`Server is running on http://localhost:4000`));
