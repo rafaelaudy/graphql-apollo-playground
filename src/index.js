098832dbae8a45874e722cb0a1ab633e13cf0a81
Rafael Audy <rafael.audy@gmail.com>
2019-02-28 15:24:01 +0530

    Adds CRUD mutations for link

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-const feed = [
+let feed = [
   { id: 1, description: "desc", url: "url" },
   { id: 2, description: "desc2", url: "url2" }
 ];
-let idCount = feed.length;
+let idCount = feed.length + 1;
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => feed
+    feed: () => feed,
+    link: (parent, { id }) => feed[id - 1]
   },
   Mutation: {
     post: (parent, { url, description }) => {
@@ -21,27 +22,26 @@ const resolvers = {
 
       feed.push(link);
       return link;
+    },
+
+    updateLink: (parent, { id, url, description }) => {
+      const updatedLink = { id, url, description };
+      feed[id - 1] = updatedLink;
+
+      return updatedLink;
+    },
+
+    deleteLink: (parent, { id }) => {
+      const index = id - 1;
+      const removedLink = feed[index];
+      feed = feed.slice(0, index).concat(feed.slice(index + 1, feed.length));
+      return removedLink;
     }
   }
 };
 
 const server = new GraphQLServer({
-  typeDefs: `
-    type Query {
-      info: String!
-      feed: [Link!]!
-    }
-    
-    type Mutation {
-      post(url: String!, description: String!): Link!
-    }
-    
-    type Link {
-      id: ID!
-      description: String!
-      url: String!
-    }
-  `,
+  typeDefs: "./src/schema.graphql",
   resolvers
 });
 
