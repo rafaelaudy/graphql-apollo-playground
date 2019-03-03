45ddcd1f7e85a0de7f7b9425e32853b7b0d14ef6
rafaelaudy <rafael.audy@gmail.com>
2019-03-03 16:03:51 +0530

    Refactor queries and mutations to use prisma

--- src/index.js
+++ src/index.js
@@ -1,48 +1,24 @@
 const { GraphQLServer } = require("graphql-yoga");
-
-let feed = [
-  { id: 1, description: "desc", url: "url" },
-  { id: 2, description: "desc2", url: "url2" }
-];
-let idCount = feed.length + 1;
+const { prisma } = require("./generated/prisma-client");
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => feed,
-    link: (parent, { id }) => feed[id - 1]
+    feed: (root, args, context) => context.prisma.links()
   },
   Mutation: {
-    post: (parent, { url, description }) => {
-      const link = {
-        id: idCount++,
+    post: (root, { url, description }, context) =>
+      context.prisma.createLink({
         url,
         description
-      };
-
-      feed.push(link);
-      return link;
-    },
-
-    updateLink: (parent, { id, url, description }) => {
-      const updatedLink = { id, url, description };
-      feed[id - 1] = updatedLink;
-
-      return updatedLink;
-    },
-
-    deleteLink: (parent, { id }) => {
-      const index = id - 1;
-      const removedLink = feed[index];
-      feed = feed.slice(0, index).concat(feed.slice(index + 1, feed.length));
-      return removedLink;
-    }
+      })
   }
 };
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers
+  resolvers,
+  context: { prisma }
 });
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
