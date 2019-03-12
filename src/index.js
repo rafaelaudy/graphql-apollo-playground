c5f6c97d558f959adc99079e19a9ec947abdfbcb
rafaelaudy <rafael.audy@gmail.com>
2019-03-12 13:15:01 +0530

    Moving query out of index

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ const { GraphQLServer } = require("graphql-yoga");
 const { prisma } = require("./generated/prisma-client");
 
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: (root, args, context) => context.prisma.links()
-  },
   Mutation: {
     post: (root, { url, description }, context) =>
       context.prisma.createLink({
