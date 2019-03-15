aee88d17638f3a5dc63fcd3c31d16908654db729
rafaelaudy <rafael.audy@gmail.com>
2019-03-15 14:24:45 +0530

    Removes mutations from index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 const { GraphQLServer } = require("graphql-yoga");
 const { prisma } = require("./generated/prisma-client");
 
-const resolvers = {
-  Mutation: {
-    post: (root, { url, description }, context) =>
-      context.prisma.createLink({
-        url,
-        description
-      })
-  }
-};
+const resolvers = {};
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
