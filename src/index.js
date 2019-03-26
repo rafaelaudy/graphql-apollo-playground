db577a863b11cf34eebb887de7f03ee21ebee5d6
rafaelaudy <rafael.audy@gmail.com>
2019-03-26 21:32:02 +0530

    Using the extracted resolvers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 const { GraphQLServer } = require("graphql-yoga");
 const { prisma } = require("./generated/prisma-client");
 
-const resolvers = {};
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
+
+const resolvers = {
+  Query,
+  Mutation,
+  User,
+  Link
+};
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
