0d65ebe03d4fb0bce0752f88685eb8a892e932b4
rafaelaudy <rafael.audy@gmail.com>
2019-03-31 11:26:02 +0530

    Adds links subscription

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ const { prisma } = require("./generated/prisma-client");
 
 const Query = require("./resolvers/Query");
 const Mutation = require("./resolvers/Mutation");
+const Subscription = require("./resolvers/Subscription");
+
 const User = require("./resolvers/User");
 const Link = require("./resolvers/Link");
 
 const resolvers = {
   Query,
   Mutation,
+  Subscription,
   User,
   Link
 };
