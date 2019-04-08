26ed9333134adb18efe7f93042895907233673a3
rafaelaudy <rafael.audy@gmail.com>
2019-04-08 22:45:20 +0530

    Adds query filter

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,16 @@
 function feed(parent, args, context, info) {
-  return context.prisma.links();
+  const where = args.filter
+    ? {
+        OR: [
+          { description_contains: args.filter },
+          { url_contains: args.filter }
+        ]
+      }
+    : {};
+
+  return context.prisma.links({
+    where
+  });
 }
 
 module.exports = {
